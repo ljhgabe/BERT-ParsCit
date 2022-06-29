5bffe29b053d38a779e227e804657fd77302afb3
LI <gabriellee0903@gmail.com>
2022-06-29 17:36:02 +0800

    Change absolute path to fit mce

--- src/datamodules/utils/datamodule_path.py
+++ src/datamodules/utils/datamodule_path.py
@@ -1,7 +1,7 @@
-SYNTHETIC_DATA_CACHE_DIR = "/data3/jiahe/synthetic-final/"
+SYNTHETIC_DATA_CACHE_DIR = "/ssd1/jiahe/synthetic-final/"
 
 SYNTHETIC_DATASET_REPO = "myvision/yuanchuan-synthetic-dataset-final"
 
-CORA_DATA_CACHE_DIR = "/data3/jiahe/cora/"
+CORA_DATA_CACHE_DIR = "/ssd1/jiahe/cora/"
 
 CORA_DATASET_REPO = "myvision/cora-dataset-final"
