a56123b7c306f0066046cf59788e51f9ef0dba8b
LI <gabriellee0903@gmail.com>
2022-07-04 16:33:01 +0800

    Add Finetuner for second stage training using cora

--- src/models/components/bert_parscit_finetuner.py
+++ src/models/components/bert_parscit_finetuner.py
@@ -1,30 +1,30 @@
+import torch
 from torch import nn
-from transformers import AutoModel
 from transformers.modeling_outputs import TokenClassifierOutput
-from src.datamodules.components.synthetic_label import num_labels
-from src.models.utils.bert_model_config import BERT_MODEL_CHECKPOINT
+from src.datamodules.components.synthetic_label import num_labels as synthetic_labels
+from src.datamodules.components.cora_label import num_labels as cora_labels
+from src.models.components.bert_token_classifier import BertTokenClassifier
+from src.models.utils.bert_model_config import BERT_PARSCIT_CHECKPOINT
 from src.models.utils.bert_model_path import MODEL_CACHE_DIR
 
 
-class BertTokenClassifier(nn.Module):
+class BertParsCitFineTuner(nn.Module):
     def __init__(
         self,
-        model_checkpoint: str = BERT_MODEL_CHECKPOINT,
-        output_size: int = num_labels,
+        model_checkpoint: str = BERT_PARSCIT_CHECKPOINT,
+        output_size: int = cora_labels,
         cache_dir: str = MODEL_CACHE_DIR
     ):
         super().__init__()
-        self.bert_embedder = AutoModel.from_pretrained(
-            model_checkpoint,
-            cache_dir=cache_dir,
-        )
+        self.bert_parscit: BertTokenClassifier = torch.load(model_checkpoint)
+        self.bert_parscit.eval()
         self.output_size = output_size
         self.dropout = nn.Dropout(0.1)
-        self.classifier = nn.Linear(in_features=768, out_features=output_size, bias=True)
+        self.classifier = nn.Linear(in_features=synthetic_labels, out_features=output_size, bias=True)
 
     def forward(self, input_ids=None, token_type_ids=None, attention_mask=None, labels=None):
         # input_ids, attention_mask, labels = inputs["input_ids"], inputs["attention_mask"], inputs["labels"]
-        outputs = self.bert_embedder(input_ids, attention_mask=attention_mask)
+        outputs = self.bert_parscit(input_ids, attention_mask=attention_mask)
         outputs = self.dropout(outputs[0])
         logits = self.classifier(outputs)
         loss = None
@@ -35,3 +35,4 @@ class BertTokenClassifier(nn.Module):
             loss=loss,
             logits=logits
         )
+
--- src/models/utils/bert_model_config.py
+++ src/models/utils/bert_model_config.py
@@ -3,3 +3,4 @@ BERT_MODEL_CHECKPOINT = "allenai/scibert_scivocab_uncased"
 
 MODEL_MAX_LENGTH = 512
 
+BERT_PARSCIT_CHECKPOINT = "/ssd1/jiahe/pretrained/scibert-synthetic-uncased-full-100k"
