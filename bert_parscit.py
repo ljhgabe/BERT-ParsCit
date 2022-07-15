45153ec308bf71d66a4413922cf56ed87c10654d
LI <gabriellee0903@gmail.com>
2022-07-15 15:52:03 +0800

    Fix bugs for inference script

--- bert_parscit.py
+++ bert_parscit.py
@@ -4,8 +4,6 @@ from torch.utils.data import DataLoader
 from transformers import DataCollatorForTokenClassification
 from collections import Counter
 from datasets import Dataset
-import transformers.tokenization_utils_base as tub
-from tokenizers import Encoding
 
 from src.models.components.bert_token_classifier import BertTokenClassifier
 from src.datamodules.components.cora_label import LABEL_NAMES
@@ -18,14 +16,11 @@ model = BertTokenClassifier(
 model.load_state_dict(torch.load("scibert-synthetic-50k-parscit.pt"))
 model.eval()
 
-en = Encoding()
-
-def postprocess(batch, predictions, label_names):
-    be = tub.BatchEncoding(batch, en)
-    word_ids = be.word_ids()
-    true_word_ids = [[id for id in word_id if id is not None] for word_id in word_ids]
+def postprocess(word_ids, predictions, label_names):
+    true_word_ids = [[id for id in word_id if id != -1] for word_id in word_ids]
     true_predictions = [
-        [label_names[p] for p in prediction if p != -100] for prediction in predictions
+        [label_names[p] for w, p in zip(word_id, prediction) if w != -1] 
+        for word_id, prediction in zip(word_ids, predictions)
     ]
 
     grouped_true_predictions = list()
@@ -54,12 +49,26 @@ def postprocess(batch, predictions, label_names):
     return merged_true_predictions
 
 
+def tokenize_and_add_word_ids(example):
+    tokenized_inputs = bert_tokenizer(
+        example["tokens"], truncation=True, is_split_into_words=True
+    )
+    all_tokens = example["tokens"]
+    all_word_ids = []
+    for i, tokens in enumerate(all_tokens):
+        word_id = tokenized_inputs.word_ids(i)
+        word_id[0], word_id[-1] = -1, -1
+        all_word_ids.append(word_id)
+    tokenized_inputs["word_ids"] = all_word_ids
+    return tokenized_inputs
+
+
 def predict_for_text(example: str):
     splitted_example = example.split()
     dict_data = {"tokens": [splitted_example]}
     dataset = Dataset.from_dict(dict_data)
     tokenized_example = dataset.map(
-        lambda x: bert_tokenizer(x["tokens"], truncation=False, is_split_into_words=True),
+        lambda x: tokenize_and_add_word_ids(x),
         batched=True,
         remove_columns=dataset.column_names
     )
@@ -73,21 +82,18 @@ def predict_for_text(example: str):
     for batch in dataloader:
         outputs = model(**batch)
         preds = outputs.logits.argmax(dim=-1)
-        # input_ids = batch["input_ids"]
+        word_ids = batch["word_ids"]
         true_preds = postprocess(
-            batch=batch,
+            word_ids=word_ids,
             predictions=preds,
             label_names=LABEL_NAMES
         )
 
-    # true_input_ids = [[id for id in input_id if id != 0 and id != 102 and id != 103] for input_id in input_ids]
-    # raw_strings = [bert_tokenizer.decode(true_input_id) for true_input_id in true_input_ids]
-    # tokens = [string.split() for string in raw_strings]
     tokens = splitted_example
     tagged_words = []
+    
     for token, label in zip(tokens, true_preds[0]):
         tagged_word = f"<{label}>{token}</{label}>"
         tagged_words.append(tagged_word)
     result = " ".join(tagged_words)
     return result
-
